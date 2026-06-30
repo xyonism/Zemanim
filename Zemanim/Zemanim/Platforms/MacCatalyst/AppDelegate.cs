6ab8b1f6ad935c14172c7358564a35d07c38bbd8
Sahar <Sahar@DESKTOP-7B49K6R>
2026-06-30 23:58:42 +0300

    x

---
+++ Zemanim/Zemanim/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Zemanim;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
